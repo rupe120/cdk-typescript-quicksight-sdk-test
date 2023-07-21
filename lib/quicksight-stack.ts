23f259cdda3902ec017f6f61438adfdc428a324a
Josh Russo <jrusso@innovativesol.com>
2023-07-20 20:04:27 -0400

    added lambdas to the stack

--- lib/quicksight-stack.ts
+++ lib/quicksight-stack.ts
@@ -1,16 +1,78 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as njsLambda from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class QuicksightStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // Start asset export function, state function task
+        const startQuicksightAssetExportFunction = new njsLambda.NodejsFunction(
+          this,
+          `quicksight-start-asset-export`,
+          {
+              functionName: `quicksight-start-asset-export`,
+              entry: 'lib/quicksight-stack-export.ts',
+              tracing: lambda.Tracing.ACTIVE,
+              insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_143_0,
+              timeout: cdk.Duration.minutes(1),
+              environment: {
+                  AWS_REGION_ID: this.region,
+                  AWS_ACCOUNT_ID: this.account,
+              },
+              bundling: {
+                  minify: false,
+                  sourceMap: true,
+              },
+          }
+      );
+      startQuicksightAssetExportFunction.addToRolePolicy(
+          new iam.PolicyStatement({
+              actions: [
+                  'quicksight:StartAssetBundleExportJob',
+                  'cloudformation:DescribeStacks',
+                  'cloudformation:ListStackResources',
+                  'tag:GetResources',
+              ],
+              resources: ['*'],
+          })
+      );
+      startQuicksightAssetExportFunction.addToRolePolicy(
+          new iam.PolicyStatement({
+              actions: ['quicksight:List*', 'quicksight:Describe*'],
+              resources: [`arn:aws:quicksight:${this.region}:${this.account}:dashboard/*`],
+          })
+      );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'QuicksightQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+        // Get export job status function, state function task
+        const getQuicksightAssetExportStatusFunction = new njsLambda.NodejsFunction(
+          this,
+          `get-quicksight-asset-export-status`,
+          {
+              functionName: `get-quicksight-asset-export-status`,
+              entry: 'lib/quicksight-stack-get-export-status.ts',
+              handler: 'handler',
+              tracing: lambda.Tracing.ACTIVE,
+              runtime: lambda.Runtime.NODEJS_18_X,
+              environment: {
+                  AWS_REGION_ID: this.region,
+                  AWS_ACCOUNT_ID: this.account,
+              },
+              bundling: {
+                  minify: false,
+                  sourceMap: true,
+              },
+              timeout: cdk.Duration.minutes(1),
+          }
+      );
+      getQuicksightAssetExportStatusFunction.addToRolePolicy(
+          new iam.PolicyStatement({
+              actions: ['quicksight:DescribeAssetBundleExportJob'],
+              resources: ['*'],
+          })
+      );
+
   }
 }
