781bb5f1640dd924c9a51c206436feb028bbe58a
Josh Russo <jrusso@innovativesol.com>
2023-07-20 20:09:59 -0400

    single duplicated lambda

--- lib/quicksight-stack.ts
+++ lib/quicksight-stack.ts
@@ -5,10 +5,11 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as njsLambda from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class QuicksightStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-        // Start asset export function, state function task
+        // Start asset export function, without runtime specified
         const startQuicksightAssetExportFunction = new njsLambda.NodejsFunction(
           this,
           `quicksight-start-asset-export`,
@@ -46,15 +47,16 @@ export class QuicksightStack extends cdk.Stack {
           })
       );
 
-        // Get export job status function, state function task
-        const getQuicksightAssetExportStatusFunction = new njsLambda.NodejsFunction(
+        // Start asset export function, without runtime specified
+        const startQuicksightAssetExportFunction2 = new njsLambda.NodejsFunction(
           this,
-          `get-quicksight-asset-export-status`,
+          `quicksight-start-asset-export`,
           {
-              functionName: `get-quicksight-asset-export-status`,
-              entry: 'lib/quicksight-stack-get-export-status.ts',
-              handler: 'handler',
+              functionName: `quicksight-start-asset-export`,
+              entry: 'lib/quicksight-stack-export.ts',
               tracing: lambda.Tracing.ACTIVE,
+              insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_143_0,
+              timeout: cdk.Duration.minutes(1),
               runtime: lambda.Runtime.NODEJS_18_X,
               environment: {
                   AWS_REGION_ID: this.region,
@@ -64,15 +66,26 @@ export class QuicksightStack extends cdk.Stack {
                   minify: false,
                   sourceMap: true,
               },
-              timeout: cdk.Duration.minutes(1),
           }
       );
-      getQuicksightAssetExportStatusFunction.addToRolePolicy(
+      startQuicksightAssetExportFunction2.addToRolePolicy(
           new iam.PolicyStatement({
-              actions: ['quicksight:DescribeAssetBundleExportJob'],
+              actions: [
+                  'quicksight:StartAssetBundleExportJob',
+                  'cloudformation:DescribeStacks',
+                  'cloudformation:ListStackResources',
+                  'tag:GetResources',
+              ],
               resources: ['*'],
           })
       );
+      startQuicksightAssetExportFunction2.addToRolePolicy(
+          new iam.PolicyStatement({
+              actions: ['quicksight:List*', 'quicksight:Describe*'],
+              resources: [`arn:aws:quicksight:${this.region}:${this.account}:dashboard/*`],
+          })
+      );
+
 
   }
 }
